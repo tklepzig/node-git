e06d3ef4a877a4afdae766b3224a0ac52fa0ffc1
Thomas <Thomas@DevBook>
2016-01-06 20:37:08 +0100

    minimal working sample

--- server.js
+++ server.js
@@ -1,30 +1,61 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
 var fs = require('fs');
+var bodyParser = require('body-parser');
 
 try {
-    fs.mkdirSync('doc-repo');
+    fs.mkdirSync(__dirname + '/doc-repo');
 } catch (ex) {
     if (ex.code !== 'EEXIST') { //throw all errors except 'directory exists already'
         throw ex;
     }
 }
 
-var git = require('simple-git')('doc-repo');
+var git = require('simple-git')(__dirname + '/doc-repo');
 
-if (fs.readdirSync('doc-repo').length === 0) {
+if (fs.readdirSync(__dirname + '/doc-repo').length === 0) {
     git.clone('https://github.com/tklepzig/cn-doc-test.git', '.');
 }
 
-fs.writeFileSync("doc-repo/document", "1");
 
-git.add(__dirname + '/doc-repo/document')
-    .then(function() {
-        console.log('add fertsch');
-    })
-    .commit('added document file')
-    .then(function() {
-        console.log('commit fertsch');
-    })
-    .push('origin', 'master')
-    .then(function() {
-        console.log('push fertsch');
-    });
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/load', function(request, response) {
+
+    var data = null;
+    try {
+        data = fs.readFileSync(__dirname + '/doc-repo/document', 'utf8');
+    } catch (ex) {
+        if (ex.code === 'ENOENT') { //file not found
+            data = '';
+        } else {
+            throw ex;
+        }
+    }
+
+    response.json(data);
+});
+
+app.post('/apply', function(request, response) {
+    fs.writeFileSync(__dirname + '/doc-repo/document', request.body.text);
+    git.add(__dirname + '/doc-repo/document')
+        .then()
+        .commit('edit document')
+        .then()
+        .push('origin', 'master')
+        .then(function() {
+            return response.json(true);
+        });
+});
+
+http.listen(51142, function() {
+    console.log('listening on *:51142');
+});
